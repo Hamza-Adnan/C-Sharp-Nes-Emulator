ba35a14bd941425e1d1bca4da34503ce334f4222
Hamza Adnan <63655114+Hamza-Adnan@users.noreply.github.com>
2022-02-13 23:25:11 -0500

    Add files via upload

---
+++ C-Sharp-NES-Emu/PPU.cs
@@ -0,0 +1,10 @@
+﻿namespace NESDOTNET
+{
+    sealed partial class PPU : Addressable_Memory
+    {   
+        public PPU(EMU_MAIN emulator) : base(emulator, 0x3FFF)
+        {
+            InitializeMemory();
+        }
+    }
+}
---
+++ C-Sharp-NES-Emu/UI.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace NESDOTNET
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UI));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBOUTToolStripMenuItem,
+            this.hELPToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(113, 48);
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.aBOUTToolStripMenuItem.Text = "ABOUT";
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // UI
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(496, 409);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "UI";
+            this.Text = "NINTENDO .NET NES";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UI_FormClosing);
+            this.Load += new System.EventHandler(this.UI_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.UI_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.UI_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UI_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UI_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UI_MouseClick);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.UI_PreviewKeyDown);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+    }
+}
